3aace8cc12f70f0237ab3f23aa339f5bd216dea0
“liadbelade” <liadb13@gmail.com>
2021-06-15 17:45:54 +0300

    styling homepage font size for mobile

--- src/componenets/RecipeCard/RecipeCard.jsx
+++ src/componenets/RecipeCard/RecipeCard.jsx
@@ -8,12 +8,12 @@ const RecipeCard = () => {
       <Link to="/recipe/id">
         <div className={styles.image}></div>
         <section className={`${styles["recipe-content"]} p-3`}>
-          <div className="recipe-header d-flex ">
+          <div className={styles["recipe-header"]}>
             <p> 15/06/2021 </p>
             <p className="mr-3"> 122 צפיות </p>
           </div>
-          <div className="recipe-description ">
-            <h4> עוגת בולונז טבעונית </h4>
+          <div className={styles["recipe-description"]}>
+            <h5> עוגת בולונז טבעונית </h5>
             <p>
               עוגת בולונז טבעונית? כן כן אתם שומעים נכון! הפסטה האהובה עליי,
               בגרסא אפויה שיוצרת מעטפת קראנצ'ית מושלמת, וכל זה ללא בשר בכלל!
